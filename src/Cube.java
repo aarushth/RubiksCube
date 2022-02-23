d7a1aedcc19926790006c69d0d11bdb1cb5f410b
aarushth <65953492+aarushth@users.noreply.github.com>
2022-02-23 10:44:27 -0800

    added function to check if top corners are solved

--- src/Cube.java
+++ src/Cube.java
@@ -419,4 +419,14 @@ public class Cube implements Cloneable{
 	public Piece getPiece(int x, int y, int z){
 		return cube[x][y][z];
 	}
+	public boolean topCornersSolved(){
+		for(int i = 0; i < 3; i+=2) {
+			for(int j = 0; j < 3; j+=2) {
+				if(!cube[i][j][2].isSolved()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
